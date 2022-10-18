20b673e5e608b5a08a2f7776db9a5a67cb49e6e7
Shun Usami <usatie@gmail.com>
2022-10-18 15:57:26 +0900

    :pencil: Add tests

--- test.sh
+++ test.sh
@@ -7,11 +7,20 @@ printf "Invalid Case 4: " && ./philo 1 1 1 99999999999999            2>/dev/null
 printf "Invalid Case 5: " && ./philo 0 2 3 4                         2>/dev/null && echo "NG" || echo "OK"
 printf "Invalid Case 6: " && ./philo 201 2 3 4                       2>/dev/null && echo "NG" || echo "OK"
 printf "Invalid Case 7: " && ./philo 99999999999999999 2 3 4         2>/dev/null && echo "NG" || echo "OK"
-printf "Invalid Case 8: " && ./philo                                 2>/dev/null && echo "NG" || echo "OK"
+printf "Invalid Case 8: " && ./philo 2 300 100 100a                  2>/dev/null && echo "NG" || echo "OK"
+printf "Invalid Case 9: " && ./philo                                 2>/dev/null && echo "NG" || echo "OK"
 
-# Valid Arguments
-printf "Valid Case 1: " &&  ./philo 1 2 3 4                                      && echo "OK" || echo "NG"
-printf "Valid Case 2: " &&  ./philo 1 2 3 4 5                                    && echo "OK" || echo "NG"
-printf "Valid Case 3: " &&  ./philo 2 200 100 100                                && echo "OK" || echo "NG"
-printf "Valid Case 4: " &&  ./philo 2 200 100 100 5                              && echo "OK" || echo "NG"
-printf "Valid Case 5: " &&  ./philo 2 200 100 100 5                              && echo "OK" || echo "NG"
+# Valid and Die
+printf "Valid Die Case 1: " &&  ./philo 1 2 3 4                                  && echo "OK" || echo "NG"
+printf "Valid Die Case 2: " &&  ./philo 1 2 3 4 5                                && echo "OK" || echo "NG"
+printf "Valid Die Case 3: " &&  ./philo 1 300 100 100                            && echo "OK" || echo "NG"
+printf "Valid Die Case 4: " &&  ./philo 2 199 100 1                              && echo "OK" || echo "NG"
+
+# Valid and Alive
+printf "Valid Alive Case 1: " &&  ./philo 2 300 100 100                          && echo "OK" || echo "NG"
+printf "Valid Alive Case 2: " &&  ./philo 2 300 100 100 5                        && echo "OK" || echo "NG"
+printf "Valid Alive Case 3: " &&  ./philo 2 300 100 100 5                        && echo "OK" || echo "NG"
+printf "Valid Alive Case 4: " &&  ./philo 2 201 100 100 5                        && echo "OK" || echo "NG"
+printf "Valid Alive Case 5: " &&  ./philo 3 201 100 100 5                        && echo "OK" || echo "NG"
+printf "Valid Alive Case 6: " &&  ./philo 2 200 100 100 5                        && echo "OK" || echo "NG"
+printf "Valid Alive Case 7: " &&  ./philo 3 200 100 100 5                        && echo "OK" || echo "NG"
